259cfec94627a155eac4636a6d15a2e61f319644
Auchan22 <agustinsurila@gmail.com>
2023-09-19 12:07:16 -0300

    *FEATURE:* Implementando validaciones form-alta

---
+++ PrimeraEntrega/index.js
@@ -0,0 +1,22 @@
+let btnSubmitPedido = document.getElementById("btn_submit_pedido");
+let selectItem = document.getElementById("item");
+let selectMesa = document.getElementById("nro_mesa");
+let inputDescripcion = document.getElementById("descripcion");
+let form = document.getElementById("form_alta")
+
+btnSubmitPedido.addEventListener("click", (e) => {
+    if(!Number(selectItem.value) || !Number(selectMesa.value)){
+        e.preventDefault();
+        if(!Number(selectItem.value) && !Number(selectMesa.value)) {alert("Se debe seleccionar un Itém y una mesa"); return;}
+        if(!Number(selectItem.value) && Number(selectMesa.value)) {alert("Se debe seleccionar un Itém"); return;}
+        if(Number(selectItem.value) && !Number(selectMesa.value)) {alert("Se debe seleccionar una mesa"); return;}
+    }
+
+    form.submit();
+})
+
+form.addEventListener("submit", () => {
+    selectItem.value = "0";
+    selectMesa.selectedIndex = "0";
+    inputDescripcion.value = "0";
+})
